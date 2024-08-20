bcbac4b272cb0bc16fb36a6cce14c7f0419c81af
hakan <hakan.sariaslan.job@gmail.com>
2024-08-20 21:44:08 +0300

    Install redux

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,16 @@
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {Provider} from "react-redux";
+import store from "./redux/store";
+
 import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
