55f1d7744409b2714d1299898985c4e886dab746
hakan <hakan.sariaslan.job@gmail.com>
2024-08-22 21:53:02 +0300

    Create header component

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,28 +1,20 @@
-import {useDispatch} from "react-redux";
-import {NavigateFunction, useNavigate} from "react-router-dom";
-import {AppDispatch} from "../app/store";
-import {logout} from "../features/auth/authSlice";
 import {IUser} from "../features/common/interfaces/IUser";
+import Header from "../components/Header";
 
 const Dashboard = () => {
-    const navigate: NavigateFunction = useNavigate();
-    const dispatch: AppDispatch = useDispatch();
     const user: IUser | null = JSON.parse(localStorage.getItem("mernUser") as string) as IUser;
-
-    const handleLogout = (): void => {
-        dispatch(logout());
-        localStorage.removeItem("mernUser");
-        navigate("/login");
-    }
 
     return (
         <div>
-            <h1>Dashboard</h1>
-            <p>Welcome, <b>{user?.username}</b></p>
-            <p>email: <b>{user?.email}</b></p>
-            <p>Roles: {user?.roles.join(', ')}</p>
-            <p>Permissions: {user?.permissions.join(', ')}</p>
-            <button onClick={handleLogout}>Logout</button>
+            <Header></Header>
+            <main>
+                <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                    <p>Welcome, <b>{user?.username}</b></p>
+                    <p>email: <b>{user?.email}</b></p>
+                    <p>Roles: {user?.roles.join(', ')}</p>
+                    <p>Permissions: {user?.permissions.join(', ')}</p>
+                </div>
+            </main>
         </div>
     );
 }
